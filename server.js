963866628f3924e05a4bc0e4338612b210b64952
TuNombre <santiagomendoza960@gmail.com>
2025-10-17 16:08:55 -0500

    Actualización de configuración

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const fs = require("fs");
 
 const app = express();
 app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 // === Inicializar Firebase Admin ===
